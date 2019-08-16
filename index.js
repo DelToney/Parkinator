8319d0e7a28e153cc5e332e5dd22f59c800d9129
Del Toney <deltontoney@gmail.com>
2019-08-16 16:37:12 -0400

    Created map and added a pin

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const express = require('express');
+
+//probably a hackey fix to get things started
+const fs = require('fs');
 const app = express();
 const path = require('path');
 
-// app.use(express.static(path.resolve('.')));
+app.use(express.static(path.resolve('.')));
 
 app.get('*', function (req, res) {
-    // res.sendFile('index.html');
-    res.end(`
-<html>
-<head>
-</head>
-<body>
-    <h1>This is Parkinator</h1>
-    <h2>IT'S IN HAH'VAH'D YAH'D</h2>
 
-</body>
-</html>
-    `);
+//          Please ignore the litter
+
+// res.sendFile('index.html');
+//     res.end(`
+// <html>
+// <head>
+// </head>
+// <body>
+//     <h1>This is Parkinator</h1>
+//     <h2>IT'S IN HAH'VAH'D YAH'D</h2>
+
+// </body>
+// </html>
+//     `);
+
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+
 });
 
 app.listen(8080, '0.0.0.0');
